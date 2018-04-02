cd1ab5508714e7e8dd83c7ee010c052d9be36071
charan1922 <charaneie22@gmail.com>
2018-04-02 18:34:35 +0530

    user auth begin

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react'
-import LinkList from './LinkList'
-import CreateLink from './CreateLink'
-import Header from './Header'
-import { Switch, Route } from 'react-router-dom'
+import React, { Component } from "react";
+import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
+import Header from "./Header";
+import { Switch, Route } from "react-router-dom";
+import Login from './Login'
 class App extends Component {
   render() {
-      return (
-        <div className="center w85">
-          <Header />
-          <div className="ph3 pv1 background-gray">
-            <Switch>
-              <Route exact path="/" component={LinkList} />
-              <Route exact path="/create" component={CreateLink} />
-            </Switch>
-          </div>
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/" component={LinkList} />
+          </Switch>
         </div>
-      )
-    }
+      </div>
+    );
   }
+}
 
-
-export default App
+export default App;
